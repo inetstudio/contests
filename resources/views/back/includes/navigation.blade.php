44617e2a116dc3158e259ca79c8f5ba673ea64f2
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-09 19:54:15 +0300

    Fix icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.contests.*') }}">
-    <a href="{{ route('back.contests.index') }}"><i class="fa fa-smile-o"></i> <span class="nav-label">Конкурсы</span></a>
+    <a href="{{ route('back.contests.index') }}"><i class="fa fa-smile"></i> <span class="nav-label">Конкурсы</span></a>
 </li>
